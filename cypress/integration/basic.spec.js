02387bd0022e6ae91bce7ba53da6db72b0b96b4e
MatheusMiranda <mpssm@cesar.org.br>
2022-12-13 16:37:47 -0300

    Verifying combos

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -18,13 +18,27 @@ describe('Cypress basics', () => {
 
         //cy.pause()
 
+        let syncTitle
+
         cy.title().should('be.equal', 'Campo de Treinamento');
         cy.title().should('contain', 'Campo')
     
-        cy.title()
-            .should('be.equal', 'Campo de Treinamento')
-            .and('contain', 'Campo');
+        cy.title().then(title => {
+            console.log(title)
+
+            cy.get('#formNome').type(title)
+
+            syncTitle = title
+        })
     
+        cy.get('[data-cy=dataSobrenome]').then($el =>{
+            $el.val(syncTitle)
+        })
+
+        cy.get('#elementosForm\\:sugestoes').then($el =>{
+            cy.wrap($el).type(syncTitle)
+        })
+        
     })
 
     it('Should find and interact with an element', () =>{
