efba18e973c994b12da57a7a5ab574bcf9d15707
MatheusMiranda <mpssm@cesar.org.br>
2022-12-15 18:02:47 -0300

    Testing prompts using stub

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -40,7 +40,8 @@ describe('Work with alerts',  ()=>{
         })
         cy.get('#confirm').click()
     })
-    it.only('Cofirm alert - Deny', () =>{
+
+    it('Cofirm alert - Deny', () =>{
         cy.on('window:confirm', msg =>{
             expect(msg).to.be.equal('Confirm Simples')
             return false
@@ -49,5 +50,44 @@ describe('Work with alerts',  ()=>{
             expect(msg).to.be.equal('Negado')
         })
         cy.get('#confirm').click()
+    })
+
+    it('Prompt', () =>{
+
+        cy.window().then(win => {
+            cy.stub(win, 'prompt').returns('42')
+        })
+
+        cy.on('window:confirm', msg =>{
+            expect(msg).to.be.equal('Era 42?')
+        })
+
+        cy.on('window:alert', msg =>{
+            expect(msg).to.be.equal(':D')
+        })
+        
+        cy.get('#prompt').click()
+    })
+
+    it.only('Verifying messages', () =>{
+        const stub = cy.stub().as('Alert')
+
+        cy.on('window:alert', stub)
+
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(0)).to.be.calledWith('Nome eh obrigatorio'))
+
+        cy.get('#formNome').type('Matheus')
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(1)).to.be.calledWith('Sobrenome eh obrigatorio'))
+
+        cy.get('[data-cy=dataSobrenome]').type('Phillip')
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(2)).to.be.calledWith('Sexo eh obrigatorio'))
+
+        cy.get('#formSexoMasc').click()
+        cy.get('#formCadastrar').click()
+
+        cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
     })
 })
